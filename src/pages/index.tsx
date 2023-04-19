57d7fd27096e61778e29619502291d9f58c9b504
Mills <mathieu.elisa@gmail.com>
2023-04-19 11:35:44 +0200

    Button created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Inter } from 'next/font/google';
 import Head from 'next/head';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <Head> {/* !!! toChange x2 !!! */}
+      <Head>
+        {' '}
+        {/* !!! toChange x2 !!! */}
         <title>!!! Title to change !!!</title>
-        <meta name="description" content="!!! Description to change !!!" />
+        <meta name="description' content='!!! Description to change !!!" />
       </Head>
 
-      <main className=" h-screen flex justify-center items-center bg-zinc-300">
+      <main className="h-screen flex justify-center items-center bg-zinc-300">
         {/* !!! Page template (keep main tag) !!! */} Hello
       </main>
     </>
