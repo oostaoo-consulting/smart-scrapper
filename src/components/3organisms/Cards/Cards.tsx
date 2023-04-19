f53c689882d3541afde09f9f5ef3cee930156633
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-04-19 16:44:20 +0200

    create card and cards components

---
+++ src/components/3organisms/Cards/Cards.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../../2molecules/Card/Card';
+
+export default function Cards() {
+  return (
+    <section className="flex flex-col gap-8">
+      <Card />
+      <Card />
+      <Card />
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
+import Cards from '../components/3organisms/Cards/Cards';
 
 export default function Home() {
   return (
     <>
-      <Head> {/* !!! toChange x2 !!! */}
+      <Head>
+        {' '}
+        {/* !!! toChange x2 !!! */}
         <title>!!! Title to change !!!</title>
         <meta name="description" content="!!! Description to change !!!" />
       </Head>
 
-      <main className=" h-screen flex justify-center items-center bg-zinc-300">
-        {/* !!! Page template (keep main tag) !!! */} Hello
+      <main className="h-screen flex justify-center items-center">
+        Hello
+        <Cards />
       </main>
     </>
   );
