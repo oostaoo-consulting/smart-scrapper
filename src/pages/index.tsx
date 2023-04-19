dc92ea3843587ecd11fe08175756aa7739f60a8a
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-04-19 14:54:12 +0200

    move title component into atoms folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Inter } from 'next/font/google';
 import Head from 'next/head';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
