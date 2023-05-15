82c3a25d282847dba9befe471f9f9877a9227d43
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-05-15 12:15:32 +0200

    FEATURE - add controlled values to input and submit handler

--- src/components/2molecules/SearchBar/SearchBar.tsx
+++ src/components/2molecules/SearchBar/SearchBar.tsx
@@ -4,10 +4,38 @@ import Input from '../../1atoms/Input/Input';
 import Button from '../../1atoms/Button/Button';
 
 export default function SearchBar() {
+  const [inputLocationValue, setInputLocationValue] = React.useState<string>('Paris');
+  const [inputSearchValue, setInputSearchValue] = React.useState<string>('');
+
+  const formSubmitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log({
+      location: inputLocationValue,
+      searchTerms: inputSearchValue,
+      quantity: 100,
+      cursorAfter: '',
+    });
+  };
+
   return (
-    <form className="w-full flex">
-      <Input />
-      <Button type="submit" className="p-2"><RxMagnifyingGlass /></Button>
+    <form className="w-full flex" onSubmit={formSubmitHandler}>
+      <Input
+        name="inputLocation"
+        title="Filtrer les résultats de recherche par ville"
+        value={inputLocationValue}
+        onChange={(event) => setInputLocationValue(event.target.value)}
+        placeholder="Filtre par ville"
+      />
+      <Input
+        name="inputSearch"
+        title="Filtrer les résultats de recherche par technologies"
+        value={inputSearchValue}
+        onChange={(event) => setInputSearchValue(event.target.value)}
+        placeholder="Filtre par technologies"
+      />
+      <Button type="submit" className="p-2">
+        <RxMagnifyingGlass />
+      </Button>
     </form>
   );
 }
