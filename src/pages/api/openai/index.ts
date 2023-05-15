0b9a428a5d8cd38c7c7859f9b7168f6d1ba27b77
Julien-Allard <julalldev@gmail.com>
2023-05-15 09:50:18 +0200

    feature/Renaming branch + more types + error handling

--- src/pages/api/openai/index.ts
+++ src/pages/api/openai/index.ts
@@ -1,4 +1,10 @@
-import { Configuration, OpenAIApi } from 'openai';
+import {
+  Configuration,
+  OpenAIApi,
+  ChatCompletionRequestMessageRoleEnum,
+  ChatCompletionRequestMessage,
+  ChatCompletionResponseMessage,
+} from 'openai';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const configuration = new Configuration({
@@ -6,20 +12,41 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  if (!req.body.message) {
+    res.status(400).json({ error: 'Un message est obligatoire !' });
+    return;
+  }
+
   const message: string = req.body.message.toString();
+  const messagesRequest: Array<ChatCompletionRequestMessage> = [
+    {
+      role: ChatCompletionRequestMessageRoleEnum.System,
+      content: 'You are a helpful assistant.',
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: message,
+    },
+  ];
+
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
-      {
-        role: 'user',
-        content: message,
-      },
-    ],
+    messages: messagesRequest,
   });
+  const chatResponse: ChatCompletionResponseMessage | undefined =
+    completion?.data?.choices[0]?.message;
 
-  res.status(200).json(completion.data.choices[0].message);
+  if (!chatResponse) {
+    res.status(500).json({
+      error: 'Une erreur est survenue avec ChatGPT, veuillez réessayer !',
+    });
+  } else {
+    res.status(200).json(chatResponse);
+  }
 };
 
 export default handler;
