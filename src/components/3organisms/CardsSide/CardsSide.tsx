5b8a3a328c0f3bca166ec7ff7070a7bd5ffb421f
Mathieu <mathieu.elisa@gmail.com>
2023-05-19 10:35:07 +0200

    update development

--- src/components/3organisms/CardsSide/CardsSide.tsx
+++ src/components/3organisms/CardsSide/CardsSide.tsx
@@ -3,27 +3,28 @@ import React from 'react';
 import Card from '../../2molecules/Card/Card';
 
 interface CardsSideProps {
-  handleOpeningCard: (event: React.MouseEvent<HTMLElement>) => void,
-  post: any,
-  indexOfFirst: any,
-  indexOfLast: any,
+  handleOpeningCard: (event: React.MouseEvent<HTMLElement>) => void;
+  post: any;
+  indexOfFirst: any;
+  indexOfLast: any;
 }
 
 function CardsSide({
-  post, indexOfFirst, indexOfLast, handleOpeningCard,
+  post,
+  indexOfFirst,
+  indexOfLast,
+  handleOpeningCard,
 }: CardsSideProps) {
   // TODO: use profilesContext hooks to get profiles, loading and error
   // const { profiles, loading, error } = useProfilesContext();
-  // if (profiles) return (profiles.map(({ login, location }) => `${login} : ${location}`));
+  // if (profiles) {
+  //   return profiles.map(({ login, location }) => `${login} : ${location}`);
+  // }
 
   return post
     .slice(indexOfFirst, indexOfLast)
     .map((id: number) => (
-      <Card
-        key={id}
-        handleOpeningCard={handleOpeningCard}
-        isFavorite={false}
-      />
+      <Card key={id} handleOpeningCard={handleOpeningCard} isFavorite={false} />
     ));
 }
 
