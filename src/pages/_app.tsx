f377d456e527aabc99ede58036ec8380d12866e5
Mathieu <mathieu.elisa@gmail.com>
2023-06-14 10:49:46 +0200

    Adding and typing Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import ProfilesProvider from '../contexts/profilesContext';
 import Container from '../components/Container/Container';
+import ReduxProvider from '../redux/provider';
 
 export const client = new ApolloClient({
   uri: '/api/graphql',
@@ -12,12 +13,14 @@ export const client = new ApolloClient({
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ApolloProvider client={client}>
-      <ProfilesProvider>
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      </ProfilesProvider>
-    </ApolloProvider>
+    <ReduxProvider>
+      <ApolloProvider client={client}>
+        <ProfilesProvider>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </ProfilesProvider>
+      </ApolloProvider>
+    </ReduxProvider>
   );
 }
